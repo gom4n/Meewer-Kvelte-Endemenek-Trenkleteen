d1e4a430d2ce2369eb52512c6aab4c4d21872367
tnix100 <61485190+tnix100@users.noreply.github.com>
2022-09-12 14:59:59 +1200

    Revert "Revert "Revert "Build site"""
    This reverts commit b0daa9d50058c228ffcb6bf392e1e27b36eb9269.

---
+++ src/lib/Post.svelte
@@ -0,0 +1,220 @@
+<!-- A post. Profile pictures not appearing while not logged in is intentional. -->
+
+<script>
+	import Container from "../lib/Container.svelte";
+	import PFP from "../lib/PFP.svelte";
+	import FormattedDate from "./FormattedDate.svelte";
+
+	import {profileData, profileClicked, user, chatid, ulist, mainPage as page} from "../lib/stores.js";
+	import {shiftHeld} from "../lib/keyDetect.js";
+	import * as clm from "../lib/clmanager.js";
+	
+	import {onMount} from "svelte";
+	export let post = {};
+
+	// TODO: make bridged tag a setting
+
+	/**
+	 * Initialize this post's user profile - gets profile info from the cache or fetches it.
+	 */
+	function initPostUser() {
+		if (!post.user) return;
+		if (!($user.name)) return;
+
+		var userName = ""
+		if (post.user == "Discord" && post.content.includes(":")) {
+			post.user = post.content.split(": ")[0];
+			post.content = post.content.slice(post.content.indexOf(": ")+1);
+		}
+		post.content = post.content.replaceAll("i", "e")
+		post.content = post.content.replaceAll("a", "e")
+		post.content = post.content.replaceAll("o", "e")
+		post.content = post.content.replaceAll("u", "e")
+		post.content = post.content.replaceAll("I", "E")
+		post.content = post.content.replaceAll("A", "E")
+		post.content = post.content.replaceAll("U", "E")
+		post.content = post.content.replaceAll("O", "E")
+		post.content = post.content.replaceAll("s", "k")
+		post.content = post.content.replaceAll("S", "K")
+		userName = post.user;
+		userName = userName.replaceAll("i", "e")
+		userName = userName.replaceAll("a", "e")
+		userName = userName.replaceAll("o", "e")
+		userName = userName.replaceAll("u", "e")
+		userName = userName.replaceAll("I", "E")
+		userName = userName.replaceAll("A", "E")
+		userName = userName.replaceAll("U", "E")
+		userName = userName.replaceAll("O", "E")
+		userName = userName.replaceAll("S", "k")
+		userName = userName.replaceAll("S", "K")
+
+		/**
+		 * Fetch the user profile and store it in the cache.
+		 */
+		const getProfile = () => {
+			let _profileData = $profileData;
+
+			if (userName === "Netefeceteen") {
+				_profileData[userName] = {
+					pfp_data: 101
+				}
+				profileData.set(_profileData);
+				return;
+			} else if (userName === "Enneencement") {
+				_profileData[userName] = {
+					pfp_data: 102
+				}
+				profileData.set(_profileData);
+				return;
+			} else if (userName === "Kerver") {
+				_profileData[userName] = {
+					pfp_data: 102
+				}
+				profileData.set(_profileData);
+				return;
+			}
+
+			_profileData[userName] = {
+				pfp_data: -1,
+			};
+			profileData.set(_profileData);
+
+			clm.meowerRequest({
+				cmd: "direct",
+				val: {
+					cmd: "get_profile",
+					val: userName,
+				},
+				listener: "get_profile_" + userName,
+			}).then(val => {
+				// Ding dong! The data has arrived.
+				_profileData[userName] = val.payload;
+				profileData.set(_profileData);
+			}).catch(e => {
+				// Uh oh - something has gone wrong.
+				_profileData[userName] = {
+					error: true,
+					pfp_data: -2,
+					temporary: true,
+				};
+				profileData.set(_profileData);
+			})
+		}
+
+		// Do we have a stored profile?
+		const _profileData = $profileData;
+		if (_profileData[userName]) {
+			// Reuse the cached data if the profile isn't temporary
+			if (_profileData[userName].temporary) {
+				getProfile();
+			}
+		} else {
+			// Get the profile!
+			getProfile();
+		}
+	};
+	onMount(initPostUser);
+</script>
+
+<Container>
+	<div class="post-header">
+		<div class="settings-controls">
+			{#if $user.name && $chatid !== "livechat" && post.user !== "Server"}	
+			{#if $user.lvl >= 1 || post.user === $user.name}
+			<button
+				class="circle close"
+				on:click={()=>{
+					if (shiftHeld || confirm("Are you sure you want to delete this post?")) {
+						clm.meowerRequest({
+							cmd: "direct",
+							val: {
+								cmd: "delete_post",
+								val: post.post_id,
+							},
+						});
+					}
+				}}
+			></button>
+			{:else}
+			<button
+				class="circle report"
+				on:click={()=>{
+					if (confirm("Are you sure you want to report this post?")) {
+						clm.meowerRequest({
+							cmd: "direct",
+							val: {
+								cmd: "report",
+								val: {
+									type: 0,
+									id: post.post_id,
+								},
+							},
+						});
+					}
+				}}
+			></button>
+			{/if}
+			{/if}
+		</div>
+		<button 
+			class="pfp" 
+			on:click={()=>{
+				if (post.user === "Notification" || post.user === "Announcement") return;
+				profileClicked.set(post.user);
+				page.set("profile");
+			}}
+		>
+			<PFP
+				icon={$profileData[post.user] ? $profileData[post.user].pfp_data : -3}
+				alt="{post.user}'s profile picture"
+				online={$ulist.includes(post.user)}
+			></PFP>
+		</button>	
+		<div class="creator">
+			<h2 class="creator">{post.user}</h2>
+			<FormattedDate date={post.date}></FormattedDate>
+		</div>
+	</div>
+	<p class="post-content">{post.content}</p>
+</Container>
+
+<style>
+	.pfp {
+		margin-right: 0.2em;
+		padding: 0;
+		border: none;
+		background: none !important;
+		color: inherit;
+	}
+	.post-header {
+		display: flex;
+		align-items: center;
+		flex-wrap: wrap;
+	}
+	.creator {
+		display: inline;
+		max-width: 100%;
+		margin-left: 0.5em;
+	}
+	.creator h2 {
+		display: block;
+		font-size: 200%;
+		margin: 0;
+	}
+	.pfp:hover:not(:active) :global(.pfp), .pfp:focus-visible :global(.pfp) {
+		transform: scale(1.1);
+	}
+	.post-content {
+		white-space: pre-wrap;
+	}
+	.settings-controls {
+		position: absolute;
+		top: 0.25em;
+		right: 0.25em;
+	}
+	button.circle {
+		border: none;
+		margin: 0;
+		margin-left: 0.125em;
+	}
+</style>
---
+++ src/screens/Main.svelte
@@ -0,0 +1,91 @@
+<!-- Where all the actual chatting action is. -->
+
+<script>
+	import Home from "./Home.svelte";
+	import Inbox from "./Inbox.svelte";
+	import Chatlist from "./Chatlist.svelte";
+	import Groupchat from "./Groupchat.svelte";
+	import Profile from "./Profile.svelte";
+	import Recent from "./Recent.svelte";
+	import Settings from "./Settings.svelte";
+	import Groupcat from "./Groupcat.svelte";	
+
+	import Sidebar from "./Sidebar.svelte";
+
+	import {user, mainPage as page} from "../lib/stores.js";
+
+	page.set("home");
+</script>
+
+<div class="main-screen">
+	<div class="sidebar">
+		<Sidebar />
+	</div>
+	<div class="view"> 
+		{#if $page === "home"}
+			<Home />
+		{:else if $page === "inbox"}
+			<Inbox />
+		{:else if $page === "chatlist"}
+			<Chatlist />
+		{:else if $page === "groupchat"}
+			<Groupchat />
+		{:else if $page === "profile"}
+			<Profile />
+		{:else if $page === "recent"}
+			<Recent />
+		{:else if $page === "settings"}
+			<Settings />
+		{:else if $page === "groupcat"}
+			<Groupcat />
+		{:else if $page === "groupchat"}
+			<Groupchat />
+		{:else if $page === "blank"}
+			<div></div>
+		{:else}
+			Semehew, yee get te e pege thet deekn't exekt...
+			<br />
+			(Cerrent pege: {$page})
+		{/if}
+	</div>
+</div>
+
+<style>
+	.main-screen {
+		box-sizing: border-box;
+	}
+
+	.sidebar {
+		position: fixed;
+		left: 0;
+		top: 0;
+		width: 3.5em;
+		height: 100%;
+		z-index: 100;
+	}
+	.view {
+		margin-left: 3.5em;
+		padding: 0.33em;
+		
+		background-color: var(--background);
+	}
+
+	@media (max-aspect-ratio: 1/1) {
+		.view {
+			margin-left: 0;
+			margin-top: 3.5em;
+		}
+		.sidebar {
+			width: 100%;
+			height: 3.5em;
+		}
+	}
+	:global(main.layout-old) .view {
+		margin-left: 0;
+		margin-top: 3.5em;
+	}
+	:global(main.layout-old) .sidebar {
+		width: 100%;
+		height: 3.5em;
+	}
+</style>
